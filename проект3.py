5d7bd7a02672edfc9cc7f486877813f9a2d5f6eb
ChudakovEvgenij <q8jg22vbyw@privaterelay.appleid.com>
2026-01-07 20:19:08 +0300

    Add files via upload

--- проект3.py
+++ проект3.py
@@ -1,5 +1,7 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import QUrl, QFileInfo
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 import sys
 
 
@@ -14,11 +16,17 @@ class menu(QMainWindow):
 
     def initUI(self):
         self.pushButton.clicked.connect(self.newW)
+        self.pushButton_2.clicked.connect(self.admin)
 
     def newW(self):
         self.hide()
         self.a = schema()
         self.a.show()
+
+    def admin(self):
+        self.hide()
+        self.b = adm()
+        self.b.show()
 
 
 class schema(QMainWindow):
@@ -28,11 +36,87 @@ class schema(QMainWindow):
         w = 661
         h = 572
         self.setFixedSize(w, h)
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton.clicked.connect(self.newW2)
+        self.pushButton_2.clicked.connect(self.lastW2)
+
+    def lastW2(self):
+        self.hide()
+        self.d = menu()
+        self.d.show()
+
+    def newW2(self):
+        self.hide()
+        self.b = model1()
+        self.b.show()
+
+class model1(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('модель 1.ui', self)
+        w = 1022
+        h = 812
+        self.setFixedSize(w, h)
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton_2.clicked.connect(self.lastW)
+        self.pushButton_3.clicked.connect(self.dow1)
+
+    def dow1(self):
+        self.hide()
+        self.d = doc1()
+        self.d.show()
+
+    def lastW(self):
+        self.hide()
+        self.c = schema()
+        self.c.show()
+
+class doc1(model1):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('dow1.ui', self)
+        w = 661
+        h = 572
+        self.setFixedSize(w, h)
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton.clicked.connect(self.newW)
+        self.pushButton_2.clicked.connect(self.admin)
+
+    def newW(self):
+        self.hide()
+        self.a = model1()
+        self.a.show()
+
+class adm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('вход.ui', self)
+        w = 661
+        h = 572
+        self.setFixedSize(w, h)
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton_2.clicked.connect(self.lastW3)
+        self.pushButton.clicked.connect(self.vchod)
+
+    def vchod(self):
+        self.parol = self.plainTextEdit.toPlainText()
+        
+
+    def lastW3(self):
+        self.hide()
+        self.a = menu()
+        self.a.show()
         
 if __name__== '__main__':
     app = QApplication(sys.argv)
     ex = menu()
     ex.show()
     sys.exit(app.exec())
-
-
